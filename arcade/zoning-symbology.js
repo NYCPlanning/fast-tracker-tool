97dab7a6b5cf8796ac3ecc599be2e0fceb9f162d
Jack Rosacker <jrosacker@planning.nyc.gov>
2025-08-25 12:50:21 -0400

    v1.1.0 changes

--- arcade/zoning-symbology.js
+++ arcade/zoning-symbology.js
@@ -1,41 +1,142 @@
-var zoning_array = Split($feature.ZONEDIST, "");
-var digits_array = Split("1234567890", ""); // To test if a character is a digit
-var first_char = zoning_array[0];
-var second_char = zoning_array[1];
-var zoning_category; // Initialize as null, assign later
-
-Console(`Zoning as array: ${zoning_array}`);
-Console(`Zoning array length: ${Count(zoning_array)}`);
-
-// Assign third character to variable, if it exists
-if (Count(zoning_array) < 3) {
-  var third_char;
-} else {
-  var third_char = zoning_array[2];
+function isCommercialOnly(zd_array) {
+  return zd_array[0] == "C";
 }
 
-Console(`First character (index 0): ${first_char}`);
-Console(`Second character (index 1): ${second_char}`);
-Console(`Third character (index 2): ${third_char}`);
+function isMixedUse(zd_array) {
+  return zd_array[0] == "M" && Includes(zd_array, "/");
+}
 
-if (first_char == "C" || first_char == "M") {
-  zoning_category = "C or M (including MX)";
-} else if (first_char == "R") {
-  // Test if 3rd character is a digit by comparing to the digits array.
-  // R10 and greater = High Density
-  if (Includes(digits_array, third_char)) {
-    zoning_category = "R5 - R10";
-    // R1 - R4 = Low Density
-  } else if (second_char >= "1" && second_char <= "4") {
-    zoning_category = "R1 - R4";
-    // R5 - R9 = High Density
-  } else if (second_char >= "5" && second_char <= "9") {
-    zoning_category = "R5 - R10";
+function isManufacturingOnly(zd_array) {
+  return zd_array[0] == "M" && !isMixedUse(zd_array);
+}
+
+function isResidence(zd_array) {
+  return zd_array[0] == "R";
+}
+
+function containsResidence(zd_array) {
+  return Includes(zd_array, "R");
+}
+
+function isDigit(character) {
+  var isDigit = Find(character, "0123456789") != -1;
+  return isDigit;
+}
+
+function getResidenceBase(zd_array) {
+  // ------------------------------------------
+  // Extracts the base from a residence zoning district, or -1 when
+  // no residence zoning is present. For example:
+  //  M1-1/R6A returns 6
+  //  R5 returns 5
+  //  Park returns -1
+  //  C8-4 returns -1
+  // Arg:
+  //  zd_array (array) --> NYC zoning district as array
+  // Return:
+  //  residence base zoning (number)
+  //   OR
+  //  -1 (when no residence zoning is present)
+  // ------------------------------------------
+  
+  // Get the index of "R" within the zoning array
+  var r_idx = IndexOf(zd_array, "R");
+  // Return -1 if no "R" is present (note this doesn't exclude "Park")
+  if (r_idx == -1) {
+    return -1;
   }
-} else {
-  // Everything else = Other
-  zoning_category = "Ineligible";
+  // Get arrays of all characters, both w/ and w/out the "R" prefix
+  var all_chars = Slice(zd_array, r_idx);
+  var after_r = Slice(all_chars, 1);
+
+  // Accumulate all consecutive numeric characters, after "R" prefix
+  var digits = "";
+  for (var i = 0; i < Count(after_r); i++) {
+    var char = after_r[i];
+    if (isDigit(char)) {
+      digits += char;
+    } else {
+      break; // Stop at first non-digit
+    }
+  }
+  // Return characters as number
+  if (digits != "") {
+    return Number(digits);
+  }
+  return -1;
 }
 
-Console(`Category: ${zoning_category}`);
-return zoning_category;
+function getZoningCategory(zoning_district) {
+  // ------------------------------------------
+  // Takes an NYC Zoning District and returns the Green Fast Track zoning category.
+  // Arg:
+  //  zoning_district (string) --> NYC zoning district (e.g. R4, M1-2/R11, Park)
+  // Return:
+  //  zoning category (string), which is one of:
+  //    R1 - R4
+  //    R5 - R10
+  //    C or M (including MX)
+  //    Other
+  // ------------------------------------------
+  var zoning_array = Split(zoning_district, "");
+
+  // Commercial or Manufacturing only
+  if (isCommercialOnly(zoning_array) || isManufacturingOnly(zoning_array)) {
+    return "C or M (including MX)";
+  }
+
+  // Mixed use
+  if (isMixedUse(zoning_array)) {
+    if (
+      containsResidence(zoning_array) && getResidenceBase(zoning_array) >= 11
+    ) {
+      return "Other";
+    }
+    return "C or M (including MX)";
+  }
+
+  // Residential
+  if (isResidence(zoning_array)) {
+    var res_base = getResidenceBase(zoning_array);
+    if (res_base <= 4) {
+      return "R1 - R4";
+    } else if (res_base < 11) {
+      return "R5 - R10";
+    } else if (res_base > 11) {
+      return "Other";
+    }
+  }
+
+  // Everything else
+  return "Other";
+}
+
+// For production ---
+var zone_dist = $feature.ZONEDIST;
+Console(zone_dist);
+getZoningCategory(zone_dist);
+// ------------------
+
+// For testing ------
+// var districts = [
+//   "R11",
+//   "C8-4",
+//   "R4",
+//   "M1-2/R11",
+//   "Park",
+//   "R12",
+//   "R1-2",
+//   "M1-2/R5",
+//   "MX-11",
+//   "R3B",
+//   "R10A",
+//   "R12D",
+//   "R3-B",
+//   "R3*Z"
+// ];
+// for (var zd in districts) {
+//   Console(
+//     `ZD: ${districts[zd]}, Category: ${getZoningCategory(districts[zd])}`
+//   );
+// }
+// ------------------
